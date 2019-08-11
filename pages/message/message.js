be01a6a875759f7bde1a60378d24600b1b8211b4
zhangji <myselfzhangji@163.com>
2019-08-11 10:06:04 +0800

    update3
    Signed-off-by: zhangji <myselfzhangji@163.com>

---
+++ pages/message/message.js
@@ -0,0 +1,95 @@
+// pages/message/message.js
+var sliderWidth = 60; // 需要设置slider的宽度，用于计算中间位置
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs: ["发出", "收到"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+
+  choseImage: function () {
+  },
+
+  openAlert: function (e) {
+    wx.showToast({
+      title: e,
+      icon: "none"
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 4,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/wish_list/wish_list.js
@@ -0,0 +1,28 @@
+// pages/signin/signin.js
+var sliderWidth = 60; // 需要设置slider的宽度，用于计算中间位置
+
+Page({
+  data: {
+    tabs: ["房源", "求租"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 4,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  }
+});
